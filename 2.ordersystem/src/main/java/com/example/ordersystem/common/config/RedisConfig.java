03293b19d4789954379f7ab32caa0f181ea6cf19
ironmask431 <ironmask431@gmail.com>
2025-08-17 20:39:04 +0900

    Revert "Revert "init commit; 구동하기 위한 파일 수정. 구동완료""
    This reverts commit 2593040703b9951244573171454ae3af3cdb0271.

--- 2.ordersystem/src/main/java/com/example/ordersystem/common/config/RedisConfig.java
+++ 2.ordersystem/src/main/java/com/example/ordersystem/common/config/RedisConfig.java
@@ -12,10 +12,10 @@ import org.springframework.data.redis.core.RedisTemplate;import org.springframew
 @Configuration
 public class RedisConfig {
 
-    @Value("${spring.redis.host}")
+    @Value("${spring.data.redis.host}")
     private String host;
 
-    @Value("${spring.redis.port}")
+    @Value("${spring.data.redis.port}")
     private int port;
 
 //    redis에 접근하기 위한 접근(connection)객체
