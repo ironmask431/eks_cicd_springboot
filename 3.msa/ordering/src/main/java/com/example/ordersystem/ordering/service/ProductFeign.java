c68ae2e72d6a5fcd5a444b3593d29f98e8267e29
ironmask431 <ironmask431@gmail.com>
2025-10-11 17:43:38 +0900

    ProductFeign - 운영용으로 수정

--- 3.msa/ordering/src/main/java/com/example/ordersystem/ordering/service/ProductFeign.java
+++ 3.msa/ordering/src/main/java/com/example/ordersystem/ordering/service/ProductFeign.java
@@ -8,8 +8,8 @@ import org.springframework.web.bind.annotation.*;
 //name은 eureka에 등록된 호출할 서비스의 이름
 //url은 k8s에서 service명 -> 이게 우선적으로 처리됨. 로컬에서는 url="http://product-service" 이걸 찾지못해서 주문시 오류 발생함.
 
-@FeignClient(name = "product-service") // 로컬용
-//@FeignClient(name = "product-service", url="http://product-service") // 운영배포용
+//@FeignClient(name = "product-service") // 로컬용
+@FeignClient(name = "product-service", url="http://product-service") // 운영배포용
 public interface ProductFeign {
 
     @GetMapping("/product/{productId}")
